8ab16e24fc1b25d19800b4a07f468904def85c64
notnoobmaster <42277794+notnoobmaster@users.noreply.github.com>
2021-03-08 23:56:48 +0200

    stuff

--- beautify_test.go
+++ beautify_test.go
@@ -9,18 +9,13 @@ import (
 	"github.com/yuin/gopher-lua/parse"
 )
 
-//go:embed test.lua
-var test string
-
-func TestOptimize(t *testing.T) {
+func TestBeautifiy(t *testing.T) {
 	chunk, err := parse.Parse(strings.NewReader(test), "")
 	if err != nil {
 		t.Fatal(err)
 	}
 	
-	Optimize(chunk)
-	
-	file, err := os.Create("optimized.lua")
+	file, err := os.Create("beautified.lua")
 
 	file.WriteString(Beautify(chunk))
 }
